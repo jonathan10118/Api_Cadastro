a5935c28c7907ed10a55b6e2c6df2ae7acd466b4
danieljoucoski <daniel.joucoski@escola.pr.gov.br>
2026-03-11 11:08:40 -0300

    feat: cadastrar clientes

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');//serviço web
+const fs = require('fs');//manipulacao de arquivos
+const path = require('path');//manipulacao de caminhos
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+/*
+CLIENTES ENDPOINTS
+*/
+
+const clientesfile = path.join(__dirname, "clientes.json");
+
+function lerClientes(){
+    if(!fs.existsSync(clientesFile)){
+        return [];
+
+    }
+    
+    const dados = fs.readFileSync(clientesFile, 'utf8');    
+
+    try{
+      return JSON.parse(dados);
+    }catch(e){
+      return [];
+    }
+}
+function salvarClientes(clientes){
+    fs.writeFileSync( clientesFile, JSON.stringify(clientes, null, 2), 'utf8');
+}
+app.post('/clientes', (req, res)=>{
+    const {cpf, nome,idade, endereco, bairro,contato} = req.body;
+
+    if (!cpf || nome || idade || endereco || bairro || contato) {
+        return res.status(400).json({ error: 'CPF ja cadastrado' });
+    }
+    const clientes = lerClientes();
+ 
+    if (clientes.some(c => c.cpf === cpf)) {
+        return res.status(400).json({ error: 'CPF ja cadastrado' });
+    }
+    const novoCliente = { cpf, nome, idade, endereco, bairro, contato };
+    clientes.push(novoCliente);
+    salvarClientes(clientes);
+
+    res.status(200).send({ message: 'Cliente cadastrado com sucesso', cliente: novoCliente });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
